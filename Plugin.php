d395527017a0d34c843029b3371cd41f4338548b
Kurt Jensen <kjensen@iaff106.com>
2015-11-08 22:02:36 -0800

    1.0.6 - Fixed ungracefull errors when rainlab.user not installed

--- Plugin.php
+++ Plugin.php
@@ -1,88 +1,134 @@
 <?php namespace KurtJensen\MyCalendar;
 
 use Backend;
-use RainLab\User\Models\User as UserModel;
+use KurtJensen\MyCalendar\Controllers\Events as EventController;
+use KurtJensen\MyCalendar\Models\Event as EventModel;
+//use October\Rain\Auth\Models\User as BackUser;
+use RainLab\User\Models\User;
 use System\Classes\PluginBase;
+use System\Classes\PluginManager;
 
 /**
  * MyCalendar Plugin Information File
  */
-class Plugin extends PluginBase
-{
+class Plugin extends PluginBase {
 
-    /**
-     * Returns information about this plugin.
-     *
-     * @return array
-     */
-    public function pluginDetails()
-    {
-        return [
-            'name' => 'MyCalendar',
-            'description' => 'Super simple calendar for displaying events.',
-            'author' => 'KurtJensen',
-            'icon' => 'icon-birthday-cake',
-        ];
-    }
+	/**
+	 * Returns information about this plugin.
+	 *
+	 * @return array
+	 */
+	public function pluginDetails() {
+		return [
+			'name' => 'MyCalendar',
+			'description' => 'Super simple calendar for displaying events.',
+			'author' => 'KurtJensen',
+			'icon' => 'icon-birthday-cake',
+		];
+	}
 
-    public function boot()
-    {
+	public function boot() {
+		$manager = PluginManager::instance();
+		if ($manager->exists('rainlab.user')) {
+			User::extend(function ($model) {
+				$model->hasMany['mycalevents'] = [
+					'KurtJensen\MyCalendar\Models\Events',
+					'table' => 'kurtjensen_mycal_events'];
+			});
 
-        UserModel::extend(function ($model) {
-            $model->hasMany['mycalevents'] = [
-                'KurtJensen\MyCalendar\Models\Events',
-                'table' => 'kurtjensen_mycal_events',
-                'key' => 'user_id',
-                'otherKey' => 'id'];
-        });
-    }
+			EventModel::extend(function ($model) {
+				$model->belongsTo['user'] = [
+					'RainLab\User\Models\User',
+					'table' => 'user',
+					'key' => 'user_id',
+					'otherKey' => 'id'];
+			});
 
-    public function registerComponents()
-    {
-        return [
-            'KurtJensen\MyCalendar\Components\Month' => 'Month',
-            //'KurtJensen\MyCalendar\Components\Week' => 'Week',
-            'KurtJensen\MyCalendar\Components\EvList' => 'EvList',
-            'KurtJensen\MyCalendar\Components\Events' => 'Events',
-            'KurtJensen\MyCalendar\Components\Event' => 'Event',
-            'KurtJensen\MyCalendar\Components\EventForm' => 'EventForm',
-        ];
-    }
+			EventController::extendFormFields(function ($form, $model, $context) {
 
-    public function registerNavigation()
-    {
-        return [
-            'mycalendar' => [
-                'label' => 'MyCalendar',
-                'icon' => 'icon-birthday-cake',
-                'url' => Backend::url('kurtjensen/mycalendar/events'),
-                'permissions' => ['kurtjensen.mycalendar.*'],
-                'order' => 500,
+				if (!$model instanceof EventModel) {
+					return;
+				}
 
-                'sideMenu' => [
-                    'events' => [
-                        'label' => 'Events',
-                        'url' => Backend::url('kurtjensen/mycalendar/events'),
-                        'icon' => 'icon-birthday-cake',
-                        'permissions' => ['kurtjensen.mycalendar.events'],
-                    ],
-                    'categories' => [
-                        'label' => 'Categories',
-                        'url' => Backend::url('kurtjensen/mycalendar/categories'),
-                        'icon' => 'icon-folder',
-                        'permissions' => ['kurtjensen.mycalendar.categories'],
-                    ],
-                ],
-            ],
-        ];
-    }
+				$form->addFields([
+					'user_id' => [
+						'label' => 'Creator',
+						'type' => 'dropdown',
+						'span' => 'right',
+					],
+				]);
+			});
 
-    public function registerPermissions()
-    {
-        return [
-            'kurtjensen.mycalendar.events' => ['label' => 'events', 'tab' => 'MyCalendar'],
-            'kurtjensen.mycalendar.categories' => ['label' => 'categories', 'tab' => 'MyCalendar'],
-        ];
-    }
+			EventController::extendListColumns(function ($lists, $model) {
+				$lists->addColumns([
+					'fname' => [
+						'label' => 'Creator First',
+						'relation' => 'user',
+						'select' => 'name',
+						'searchable' => 'true',
+						'sortable' => 'true',
+					],
+					'lname' => [
+						'label' => 'Creator Last',
+						'relation' => 'user',
+						'select' => 'surname',
+						'searchable' => 'true',
+						'sortable' => 'true',
+					],
+				]);
+			});
+		}
+
+	}
+
+	public function registerComponents() {
+		return [
+			'KurtJensen\MyCalendar\Components\Month' => 'Month',
+			//'KurtJensen\MyCalendar\Components\Week' => 'Week',
+			'KurtJensen\MyCalendar\Components\EvList' => 'EvList',
+			'KurtJensen\MyCalendar\Components\Events' => 'Events',
+			'KurtJensen\MyCalendar\Components\Event' => 'Event',
+			'KurtJensen\MyCalendar\Components\EventForm' => 'EventForm',
+		];
+	}
+
+	public function registerNavigation() {
+		$navMenu = [
+			'mycalendar' => [
+				'label' => 'MyCalendar',
+				'icon' => 'icon-birthday-cake',
+				'url' => Backend::url('kurtjensen/mycalendar/events'),
+				'permissions' => ['kurtjensen.mycalendar.*'],
+				'order' => 500,
+
+				'sideMenu' => [
+					'categories' => [
+						'label' => 'Categories',
+						'url' => Backend::url('kurtjensen/mycalendar/categories'),
+						'icon' => 'icon-folder',
+						'permissions' => ['kurtjensen.mycalendar.categories'],
+					],
+				],
+			],
+		];
+
+		if (class_exists('UserModel')) {
+			$navMenu['mycalendar']['sideMenu']['events'] = [
+				'label' => 'Events',
+				'url' => Backend::url('kurtjensen/mycalendar/events'),
+				'icon' => 'icon-birthday-cake',
+				'permissions' => ['kurtjensen.mycalendar.events'],
+			];
+		}
+
+		return $navMenu;
+	}
+
+	public function registerPermissions() {
+		return [
+			'kurtjensen.mycalendar.events' => ['label' => 'events', 'tab' => 'MyCalendar'],
+			'kurtjensen.mycalendar.categories' => ['label' => 'categories', 'tab' => 'MyCalendar'],
+		];
+	}
 
 }
--- models/Event.php
+++ models/Event.php
@@ -1,42 +1,42 @@
 <?php namespace KurtJensen\MyCalendar\Models;
 
 use Model;
-use RainLab\User\Models\User;
+use RainLab\User\Models\User as UserModel;
+use System\Classes\PluginManager;
 
 /**
  * event Model
  */
-class Event extends Model
-{
+class Event extends Model {
 
-    /**
-     * @var string The database table used by the model.
-     */
-    public $table = 'kurtjensen_mycal_events';
+	/**
+	 * @var string The database table used by the model.
+	 */
+	public $table = 'kurtjensen_mycal_events';
 
-    /**
-     * @var array Guarded fields
-     */
-    protected $guarded = [];
+	/**
+	 * @var array Guarded fields
+	 */
+	protected $guarded = [];
 
-    /**
-     * @var array Fillable fields
-     */
-    protected $fillable = ['*'];
+	/**
+	 * @var array Fillable fields
+	 */
+	protected $fillable = ['*'];
 
-    /**
-     * @var array Relations
-     */
-
-    public $belongsTo = [
-        'user' => ['RainLab\User\Models\User',
-            'key' => 'user_id',
-            'otherKey' => 'id'],
-    ];
-
-    public $belongsToMany = [
-        'categories' => ['KurtJensen\MyCalendar\Models\Category', 'table' => 'kurtjensen_mycal_events_categories', 'key' => 'events_id', 'otherKey' => 'category_id'],
-    ];
+	/**
+	 * @var array Relations
+	 */
+/*
+public $belongsTo = [
+'user' => ['RainLab\User\Models\User',
+'key' => 'user_id',
+'otherKey' => 'id'],
+];
+ */
+	public $belongsToMany = [
+		'categories' => ['KurtJensen\MyCalendar\Models\Category', 'table' => 'kurtjensen_mycal_events_categories', 'key' => 'events_id', 'otherKey' => 'category_id'],
+	];
 /*
 id
 user_id
@@ -47,68 +47,65 @@ year
 text
 is_published
  */
-    public $attributes = [
-        'date' => '',
-        'human_time' => '',
-    ];
+	public $attributes = [
+		'date' => '',
+		'human_time' => '',
+	];
 
-    public function getDateAttribute()
-    {
-        if (!$this->year) {
-            return date('Y-m-d');
-        }
+	public function getDateAttribute() {
+		if (!$this->year) {
+			return date('Y-m-d');
+		}
 
-        return $this->year . '-' . $this->month . '-' . $this->day;
-    }
+		return $this->year . '-' . $this->month . '-' . $this->day;
+	}
 
-    public function getHumanTimeAttribute()
-    {
-        if (!$this->time) {
-            return '';
-        }
-        list($h, $m) = explode(':', $this->time);
-        $time = ($h > 12 ? ($h - 12) : intval($h)) . ':' . $m . ($h > 11 ? 'pm' : 'am');
-        return $time;
-    }
+	public function getHumanTimeAttribute() {
+		if (!$this->time) {
+			return '';
+		}
+		list($h, $m) = explode(':', $this->time);
+		$time = ($h > 12 ? ($h - 12) : intval($h)) . ':' . $m . ($h > 11 ? 'pm' : 'am');
+		return $time;
+	}
 
-    public function beforeSave()
-    {
-        list($this->year, $this->month, $this->day) = explode('-', $this->attributes['date']);
+	public function beforeSave() {
+		list($this->year, $this->month, $this->day) = explode('-', $this->attributes['date']);
 
-        unset($this->attributes['date'], $this->attributes['human_time']);
-    }
+		unset($this->attributes['date'], $this->attributes['human_time']);
+	}
 
-    public function getDayOptions($month)
-    {
-        if ($this->month && $this->year) {
-            $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $this->month, $this->year);
-            $days = range(1, $daysInMonth);
-            return array_combine($days, $days);
-        }
-        return [0 => 'Pick a Month AND Year'];
-    }
+	public function getDayOptions($month) {
+		if ($this->month && $this->year) {
+			$daysInMonth = cal_days_in_month(CAL_GREGORIAN, $this->month, $this->year);
+			$days = range(1, $daysInMonth);
+			return array_combine($days, $days);
+		}
+		return [0 => 'Pick a Month AND Year'];
+	}
 
-    public function getMonthOptions()
-    {
-        $months = ['0', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        unset($months[0]);
-        return $months;
-    }
+	public function getMonthOptions() {
+		$months = ['0', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+		unset($months[0]);
+		return $months;
+	}
 
-    public function getYearOptions()
-    {
-        $year = date('Y');
-        $years = range($year, $year + 5);
-        return array_combine($years, $years);
-    }
+	public function getYearOptions() {
+		$year = date('Y');
+		$years = range($year, $year + 5);
+		return array_combine($years, $years);
+	}
 
-    public function getUserIdOptions($keyValue = null)
-    {
-        foreach (User::orderBy('name')->get() as $user) {
-            $Users[$user->id] = $user->surname . ', ' . $user->name;
-        }
+	public function getUserIdOptions($keyValue = null) {
+		$manager = PluginManager::instance();
+		if ($manager->exists('rainlab.user')) {
+			foreach (UserModel::orderBy('name')->get() as $user) {
+				$Users[$user->id] = $user->surname . ', ' . $user->name;
+			}
 
-        return $Users;
-    }
+			return $Users;
+		}
+		return [0 => 'Rainlab User Model Not Installed'];
+	}
 
 }
