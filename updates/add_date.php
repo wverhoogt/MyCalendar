649a992ed0d6119493b35193b169eb3b45174ff7
Kurt Jensen <kjensen@iaff106.com>
2016-04-01 14:46:35 -0700

    1.1.16: Fixed concat() error for SQLITE users in add_date.php.

--- updates/add_date.php
+++ updates/add_date.php
@@ -10,10 +10,20 @@ class AddDate extends Migration {
 			$table->date('date')->nullable()->after('day');
 		});
 
-		DB::table('kurtjensen_mycal_events as enew')
-			->join('kurtjensen_mycal_events as eold', 'eold.id', '=', 'enew.id')
-			->update(['enew.date' => DB::raw('CONCAT(eold.year, "-", eold.month, "-",eold.day)')]);
+		$count = DB::table('kurtjensen_mycal_events')->count();
 
+		if ($count) {
+
+			$q = DB::table('kurtjensen_mycal_events as enew')
+				->join('kurtjensen_mycal_events as eold', 'eold.id', '=', 'enew.id');
+
+			if (config('database.default') == 'sqlite') {
+				$q->update(['enew.date' => DB::raw("year || '-' || month || '-' || day")]);
+
+			} else {
+				$q->update(['enew.date' => DB::raw('CONCAT(eold.year, "-", eold.month, "-",eold.day)')]);
+			}
+		}
 	}
 
 	public function down() {
