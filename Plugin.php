c6394ca361a35d2f5b36cbca4105ab1eb1d1cbfc
Kurt Jensen <kjensen@iaff106.com>
2017-03-04 09:27:18 -0800

    1.2.10 - Made Builder compatible.  Updated a few translation strings.

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,5 @@
 <?php namespace KurtJensen\MyCalendar;
 
-use Backend;
 use KurtJensen\MyCalendar\Controllers\Events as EventController;
 use KurtJensen\MyCalendar\Models\Category as CategoryModel;
 use KurtJensen\MyCalendar\Models\Event as EventModel;
@@ -12,20 +11,6 @@ use System\Classes\PluginManager;
  * MyCalendar Plugin Information File
  */
 class Plugin extends PluginBase {
-
-	/**
-	 * Returns information about this plugin.
-	 *
-	 * @return array
-	 */
-	public function pluginDetails() {
-		return [
-			'name' => 'kurtjensen.mycalendar::lang.plugin.name',
-			'description' => 'kurtjensen.mycalendar::lang.plugin.description',
-			'author' => 'KurtJensen',
-			'icon' => 'icon-birthday-cake',
-		];
-	}
 
 	public function messageURL() {
 		return 'http://firemankurt.com/notices/';
@@ -106,69 +91,6 @@ class Plugin extends PluginBase {
 			'KurtJensen\MyCalendar\Components\Events' => 'Events',
 			'KurtJensen\MyCalendar\Components\Event' => 'Event',
 			'KurtJensen\MyCalendar\Components\EventForm' => 'EventForm',
-		];
-	}
-
-	public function registerNavigation() {
-		$navMenu = [
-			'mycalendar' => [
-				'label' => 'kurtjensen.mycalendar::lang.plugin.name',
-				'icon' => 'icon-birthday-cake',
-				'url' => Backend::url('kurtjensen/mycalendar/events'),
-				'permissions' => ['kurtjensen.mycalendar.*'],
-				'order' => 500,
-
-				'sideMenu' => [
-					'events' => [
-						'label' => 'kurtjensen.mycalendar::lang.events.menu_label',
-						'icon' => 'icon-birthday-cake',
-						'url' => Backend::url('kurtjensen/mycalendar/events'),
-						'permissions' => ['kurtjensen.mycalendar.events'],
-						'order' => 400],
-					'categories' => [
-						'label' => 'kurtjensen.mycalendar::lang.categories.menu_label',
-						'url' => Backend::url('kurtjensen/mycalendar/categories'),
-						'icon' => 'icon-folder',
-						'permissions' => ['kurtjensen.mycalendar.categories'],
-						'order' => 500,
-					],
-					'occurrences' => [
-						'label' => 'kurtjensen.mycalendar::lang.occurrences.menu_label',
-						'url' => Backend::url('kurtjensen/mycalendar/occurrences'),
-						'icon' => 'icon-flask',
-						'permissions' => ['kurtjensen.mycalendar.occurrences'],
-						'order' => 500,
-					],
-					'settings' => [
-						'label' => 'kurtjensen.mycalendar::lang.settings.menu_label',
-						'url' => Backend::url('system/settings/update/kurtjensen/mycalendar/settings'),
-						'icon' => 'icon-gear',
-						'code' => 'settings',
-						'permissions' => ['kurtjensen.mycalendar.settings'],
-						'order' => 800,
-					],
-				],
-			],
-		];
-
-		if (class_exists('UserModel')) {
-			$navMenu['mycalendar']['sideMenu']['events'] = [
-				'label' => 'Events',
-				'url' => Backend::url('kurtjensen/mycalendar/events'),
-				'icon' => 'icon-birthday-cake',
-				'permissions' => ['kurtjensen.mycalendar.events'],
-			];
-		}
-
-		return $navMenu;
-	}
-
-	public function registerPermissions() {
-		return [
-			'kurtjensen.mycalendar.events' => ['label' => 'kurtjensen.mycalendar::lang.plugin.access_events', 'tab' => 'kurtjensen.mycalendar::lang.plugin.tab'],
-			'kurtjensen.mycalendar.categories' => ['label' => 'kurtjensen.mycalendar::lang.plugin.access_categories', 'tab' => 'kurtjensen.mycalendar::lang.plugin.tab'],
-			'kurtjensen.mycalendar.occurrences' => ['label' => 'kurtjensen.mycalendar::lang.plugin.access_occurrences', 'tab' => 'kurtjensen.mycalendar::lang.plugin.tab'],
-			'kurtjensen.mycalendar.settings' => ['label' => 'kurtjensen.mycalendar::lang.plugin.access_settings', 'tab' => 'kurtjensen.mycalendar::lang.plugin.tab'],
 		];
 	}
 
